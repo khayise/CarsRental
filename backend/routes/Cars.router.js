e05f56a44e4a1169a050a22c3ff7e3182194ad8d
khayise <dedesh.deu@gmail.com>
2025-01-05 20:29:09 +0100

    11

--- backend/routes/Cars.router.js
+++ backend/routes/Cars.router.js
@@ -6,5 +6,6 @@ const CarsRouter = express.Router();
 CarsRouter.get('/cars', (req, res) => carsController.getAll(req, res));
 CarsRouter.get('/cars/:id', (req, res) => carsController.getById(req, res));
 CarsRouter.get('/maxPrice', (req, res) => carsController.getMaxCarPrice(req, res));
+CarsRouter.get('/test', (req, res) => res.status(200).send('passed'));
 
 module.exports = CarsRouter;
