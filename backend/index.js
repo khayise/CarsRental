ca2d82981c5cd7127076c1cf0e2604c665bd4a12
khayise <dedesh.deu@gmail.com>
2025-01-05 19:06:03 +0100

    final fix

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,22 @@
-const express = require('express')
+const express = require('express');
+
+require('dotenv').config();
 const mongoose = require('mongoose');
-const cors = require('cors')
-const carsRouter = require('./routes/Cars.router')
+const cors = require('cors');
+const carsRouter = require('./routes/Cars.router');
 
+const app = express();
+app.use(express.json());
+app.use(cors());
 
+const PORT = 3000;
+const DB_URL = process.env.DATABASE_URL;
 
-
-const app = express()
-app.use(express.json())
-app.use(cors())
-
-
-const PORT = 3000
-const DB_URL = 'mongodb+srv://dedeshd:dedeshd@datebase.kzgfvbx.mongodb.net/CarsRent?retryWrites=true&w=majority'
-
-app.listen(PORT, () => console.log('server was started on port ' + PORT))
-app.use(carsRouter)
+app.listen(PORT, () => console.log('server was started on port ' + PORT));
+app.use(carsRouter);
 // app.get('/', (req, res) => res.status(200).send)
 
-mongoose.connect(DB_URL)
-.then(() => console.log('conected to db'))
-.catch((e) => console.log('mongo error: '+e.message))
+mongoose
+	.connect(DB_URL)
+	.then(() => console.log('conected to db'))
+	.catch((e) => console.log('mongo error: ' + e.message));
